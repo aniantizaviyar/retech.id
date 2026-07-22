3a8292554296ad818809ca2fb8772fa8935a976a
RETECH <sales@retech.id>
2026-07-22 10:26:55 +0700

    Add Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
-import { GoogleAnalytics } from "@next/third-parties/google";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
@@ -34,7 +34,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
         <Analytics />
-        <GoogleAnalytics gaId="G-KGE6D1FX29" />
+        <GoogleTagManager gtmId="GTM-MX3X63MF" />
       </body>
     </html>
   );
